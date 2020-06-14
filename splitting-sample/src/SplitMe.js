1f58a6eecb07fdc547bcec464b316f7d139adbbb
thilllon <thilllon970@gmail.com>
2020-06-15 05:08:12 +0900

    splitting-sample 추가

---
+++ splitting-sample/src/SplitMe.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const SplitMe = () => {
+    return <div>SplitMe</div>;
+};
+export default SplitMe;
---
+++ splitting-sample/src/notify.js
@@ -0,0 +1,8 @@
+const showDate = function () {
+    console.log(new Date());
+};
+const notify = function () {
+    alert('Hello');
+};
+export default notify;
+export { showDate };
