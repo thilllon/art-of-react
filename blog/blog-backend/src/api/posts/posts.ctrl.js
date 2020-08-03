baf9f690d606898f8dd809e37c31799b283f8d63
thilllon <thilllon970@gmail.com>
2020-08-03 22:17:47 +0900

    .

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,3 +1,5 @@
+import Post from '../../models/post.js';
+
 let postId = 1; // initial value
 const posts = [
   {
@@ -9,111 +11,113 @@ const posts = [
 
 const returnError = (errorStatusCode) => {};
 
-// 포스트 작성
-// POST /api/posts
-// {title, body}
-export const write = (ctx) => {
-  const { title, body } = ctx.request.body;
-  postId += 1;
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.body = post;
-};
+posts.post('/', postsCtrl.write);
 
-// 포스트 목록 조회
-// GET /api/posts
+// // 포스트 작성
+// // POST /api/posts
+// // {title, body}
+// export const write = (ctx) => {
+//   const { title, body } = ctx.request.body;
+//   postId += 1;
+//   const post = { id: postId, title, body };
+//   posts.push(post);
+//   ctx.body = post;
+// };
 
-export const list = (ctx) => {
-  ctx.body = posts;
-};
+// // 포스트 목록 조회
+// // GET /api/posts
 
-// 특정 포스트 조회
-// GET /api/posts/:id
+// export const list = (ctx) => {
+//   ctx.body = posts;
+// };
 
-export const read = (ctx) => {
-  const { id } = ctx.params;
-  // 주어진 id값으로 포스트를 찾음
-  // 파라미터로 받아온 값은 문자열 형식
-  // postId를 문자열로 변경필요
-  const post = posts.find((p) => p.id.toString() === id);
-  // 포스트가 없으면 오류 반환
-  if (!post) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
-  }
+// // 특정 포스트 조회
+// // GET /api/posts/:id
 
-  ctx.body = post;
-};
+// export const read = (ctx) => {
+//   const { id } = ctx.params;
+//   // 주어진 id값으로 포스트를 찾음
+//   // 파라미터로 받아온 값은 문자열 형식
+//   // postId를 문자열로 변경필요
+//   const post = posts.find((p) => p.id.toString() === id);
+//   // 포스트가 없으면 오류 반환
+//   if (!post) {
+//     ctx.status = 404;
+//     ctx.body = {
+//       message: '포스트가 존재하지 않습니다.',
+//     };
+//     return;
+//   }
 
-// 특정 포스트제거
-// DELETE /api/posts/:id
+//   ctx.body = post;
+// };
 
-export const remove = (ctx) => {
-  const { id } = ctx.params;
-  // 해당id를 가진 포스트가 몇 번째인지 확인
-  const index = posts.findIndex((p) => p.id.toString() === id);
-  // 포스트가 없으면 오류 반환
-  if (index < 0) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
-  }
-  // index번째 아이템을 제거합니다.
-  posts.splice(index, 1);
-  ctx.status = 204; // No Content
-};
+// // 특정 포스트제거
+// // DELETE /api/posts/:id
 
-// 포스트 수정(교체)
-// PUT /api/posts/:id
-// {title, body}
-export const replace = (ctx) => {
-  // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할때 사용합니다.
-  const { id } = ctx.params;
-  // 해당 id를 가진 포스트가 몇 번째인지 확인
-  const index = posts.findIndex((p) => p.id.toString() === id);
+// export const remove = (ctx) => {
+//   const { id } = ctx.params;
+//   // 해당id를 가진 포스트가 몇 번째인지 확인
+//   const index = posts.findIndex((p) => p.id.toString() === id);
+//   // 포스트가 없으면 오류 반환
+//   if (index < 0) {
+//     ctx.status = 404;
+//     ctx.body = {
+//       message: '포스트가 존재하지 않습니다.',
+//     };
+//     return;
+//   }
+//   // index번째 아이템을 제거합니다.
+//   posts.splice(index, 1);
+//   ctx.status = 204; // No Content
+// };
 
-  //포스트가 없으면 오류 반환
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
-  }
-  // 객체를 덮어씌움
-  posts[index] = {
-    id,
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
-};
+// // 포스트 수정(교체)
+// // PUT /api/posts/:id
+// // {title, body}
+// export const replace = (ctx) => {
+//   // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할때 사용합니다.
+//   const { id } = ctx.params;
+//   // 해당 id를 가진 포스트가 몇 번째인지 확인
+//   const index = posts.findIndex((p) => p.id.toString() === id);
 
-// 포스트 수정(특정 필드 변경)
-// PATCH /api/posts/:id
-// {title, body}
-export const update = (ctx) => {
-  // PATCH 메서드는 주어진 필드만 교체합니다.
-  const { id } = ctx.params;
-  // 해당 id를 가진 post가 몇 번째인지 확인합니다.
-  const index = posts.findIndex((p) => p.id.toString() === id);
-  // 포스트가 없으면 오류를 반환
+//   //포스트가 없으면 오류 반환
+//   if (index === -1) {
+//     ctx.status = 404;
+//     ctx.body = {
+//       message: '포스트가 존재하지 않습니다.',
+//     };
+//     return;
+//   }
+//   // 객체를 덮어씌움
+//   posts[index] = {
+//     id,
+//     ...ctx.request.body,
+//   };
+//   ctx.body = posts[index];
+// };
 
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
-  }
-  //기존 값에 정보를 덮어 씌웁니다.
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
-};
+// // 포스트 수정(특정 필드 변경)
+// // PATCH /api/posts/:id
+// // {title, body}
+// export const update = (ctx) => {
+//   // PATCH 메서드는 주어진 필드만 교체합니다.
+//   const { id } = ctx.params;
+//   // 해당 id를 가진 post가 몇 번째인지 확인합니다.
+//   const index = posts.findIndex((p) => p.id.toString() === id);
+//   // 포스트가 없으면 오류를 반환
+
+//   if (index === -1) {
+//     ctx.status = 404;
+//     ctx.body = {
+//       message: '포스트가 존재하지 않습니다.',
+//     };
+//     return;
+//   }
+//   //기존 값에 정보를 덮어 씌웁니다.
+//   posts[index] = {
+//     ...posts[index],
+//     ...ctx.request.body,
+//   };
+//   ctx.body = posts[index];
+// };
