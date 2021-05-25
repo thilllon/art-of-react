f11a7263f6c07942a8779dc1bd4bd2c42d858e68
thilllon <thilllon970@gmail.com>
2021-05-25 11:54:49 +0900

    rename todo list project

---
+++ todo-list/src/components/code-split/SplitMe.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const SplitMe = () => {
+  return <div>SplitMe</div>;
+};
--- todo-list/src/components/todo-list/TodoForm.jsx
+++ todo-list/src/components/todo-list/TodoForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './TodoListTemplate.css';
 
-export const Form = ({ value, onKeyPress, onChange, onCreate }) => {
+export const TodoForm = ({ value, onKeyPress, onChange, onCreate }) => {
   const onSubmit = (ev) => ev.preventDefault();
   return (
     <form onSubmit={onSubmit}>
